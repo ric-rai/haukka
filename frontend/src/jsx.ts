e6e78edd694fd3c9f5b3f9e539b136d2935e1012
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-06 17:30:19 +0200

    add typings for intrinsic elements

---
+++ frontend/src/jsx.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  /**
+   * Includes typings for intrinsic JSX elements.
+   *
+   * @see {https://www.typescriptlang.org/docs/handbook/jsx.html}
+   */
+  namespace JSX {
+    type Children = (Element | string | null)[] | Element | string | null;
+
+    interface IntrinsicElements {
+      [key: string]: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLSpanElement>,
+        HTMLSpanElement
+      > & { class?: string };
+    }
+  }
+}
+
+export default JSX;

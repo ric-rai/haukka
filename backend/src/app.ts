923f673d92b2511f6315262c44ff9bb4759c07cf
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-07 21:05:29 +0200

    set port from env file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
+import * as dotenv from "dotenv";
 import * as express from "express";
+import * as path from "path";
 import { initialize } from "express-openapi";
 import { apiDoc } from "./api/v1/apiDoc";
 
+const { NODE_ENV } = process.env;
+const envPath = NODE_ENV === "development" ? ".env.development" : ".env.production";
+dotenv.config({ path: path.resolve(process.cwd(), `./${envPath}`) });
+
 const app = express();
+const port = process.env.PORT || 8080;
 
 initialize({
   app,
@@ -10,6 +17,6 @@ initialize({
   paths: "./dist/api/v1/paths",
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(8080, () => {
+  console.log(`Listening on port ${port}`);
 });

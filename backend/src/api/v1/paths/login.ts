cc2fc2ddb81588a4651c97a1811b4d58b92b85ea
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-07 21:06:36 +0200

    add authentication logic

--- backend/src/api/v1/paths/login.ts
+++ backend/src/api/v1/paths/login.ts
@@ -1,16 +1,34 @@
 import { Operation } from "express-openapi";
 import * as jsonwebtoken from "jsonwebtoken";
 
+const { NODE_ENV, STATIC_SERVER_PORT, TARGET } = process.env;
 const JWT_SECRET = "goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu";
+const host = NODE_ENV === "development" ? `http://localhost:${STATIC_SERVER_PORT}` : "";
 
 export const GET: Operation = (req, res, next) => {
-  res.redirect("/?token=" + jsonwebtoken.sign({ user: "test" }, JWT_SECRET));
+  if (req.query.token) {
+    // Make HTTP request to laji to fetch user data
+    return res.redirect(
+      `${host}/#/?jwt=${jsonwebtoken.sign(
+        {
+          user: "Test",
+          id: "123",
+          email: "test@domain.test",
+        },
+        JWT_SECRET
+      )}`
+    );
+  }
+  return res.redirect(
+    `https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?` +
+      `target=${TARGET}&redirectMethod=GET&next=`
+  );
 };
 
 const token = {
   in: "query",
   name: "token",
-  required: true,
+  required: false,
   schema: {
     type: "string",
   },

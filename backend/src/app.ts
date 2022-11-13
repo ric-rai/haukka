58e877e77dbc0b28e21f88bf1f49e820deafd35e
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-13 19:50:36 +0200

    update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,23 @@
 import * as dotenv from "dotenv";
 import * as express from "express";
 import * as path from "path";
+import oracledb = require("oracledb");
+import { createSchema } from "./schema/create";
+import { Observatory } from "./services/observatory/observatory";
+import { MetadataService } from "./services/metadata/metadata";
+import { Account } from "./services/account/account";
 import { initialize } from "express-openapi";
 import { apiDoc } from "./api/v1/apiDoc";
-import oracledb = require("oracledb");
-import { Observatory } from "./services/observatory/observatory";
-import { Metadata } from "./services/metadata/metadata";
-import { createSchema } from "./schema/create";
 
 const { NODE_ENV } = process.env;
 const envPath = NODE_ENV === "development" ? ".env.development" : ".env.production";
 dotenv.config({ path: path.resolve(process.cwd(), `./${envPath}`) });
-const { USERNAME, PASSWORD, HOSTNAME, SERVICE_ID } = process.env;
+const { USERNAME, PASSWORD, HOSTNAME, SERVICE_ID, PORT } = process.env;
+
+const app = express();
+const port = PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 (async () => {
   const pool = await oracledb.createPool({
@@ -21,19 +27,22 @@ const { USERNAME, PASSWORD, HOSTNAME, SERVICE_ID } = process.env;
     connectString: `${HOSTNAME}/${SERVICE_ID}`,
   });
   await createSchema(pool);
-  const metadata = await Metadata();
-  const observatory = Observatory(pool, metadata);
-})().catch((err) => console.error(err));
+  const metadataService = await MetadataService();
+  const observatory = await Observatory(pool, metadataService);
+  const account = await Account(pool, metadataService);
 
-const app = express();
-const port = process.env.PORT || 8080;
-
-initialize({
-  app,
-  apiDoc: apiDoc,
-  paths: "./dist/api/v1/paths",
+  initialize({
+    app,
+    apiDoc: apiDoc,
+    dependencies: {
+      account,
+    },
+    paths: "./dist/api/v1/paths",
+  });
+})().catch((err) => {
+  console.error(err);
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.info(`Listening on port ${port}`);
 });

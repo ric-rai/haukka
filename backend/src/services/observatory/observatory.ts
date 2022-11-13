b7de629a51f973e46569c5e44c9b2f133a365ff2
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-13 19:47:04 +0200

    wrap sql query with object with typed methods

--- backend/src/services/observatory/observatory.ts
+++ backend/src/services/observatory/observatory.ts
@@ -1,24 +1,29 @@
 import oracledb = require("oracledb");
 import { parseStatements } from "../../utils";
 import * as path from "path";
-import type { Metadata } from "../metadata/metadata";
+import type { MetadataService } from "../metadata/metadata";
 import { locations } from "../../locations";
 
 const statements = parseStatements(path.join(__dirname, "./observatory.sql"));
 
 export type Observatory = Awaited<ReturnType<typeof Observatory>>;
 
-export const Observatory = async (pool: oracledb.Pool, metadata: Metadata) => {
-  const connection = await pool.getConnection();
+export const Observatory = async (pool: oracledb.Pool, metadataService: MetadataService) => {
+  const SQL = (cnx: oracledb.Connection) => ({
+    insert: async (name: string, actions: string) => {
+      return await cnx.execute(statements.INSERT, {
+        metadata: await metadataService.create(cnx),
+        ...{ name, actions },
+      });
+    },
+  });
+
+  const cnx = await pool.getConnection();
   for (const observatory of locations.observatories) {
     let name: string | null = null;
     try {
       ({ observatory: name } = observatory);
-      await connection.execute(statements.INSERT, {
-        name,
-        metadata: await metadata.create(connection),
-        actions: "testactions",
-      });
+      await SQL(cnx).insert(name, "test_actions");
     } catch (err) {
       const error = err as { message: string };
       if (error?.message.includes("(APP.OBSERVATORY_NAME_IS_UNIQUE) violated")) {
@@ -28,7 +33,8 @@ export const Observatory = async (pool: oracledb.Pool, metadata: Metadata) => {
       throw new Error(error?.message);
     }
   }
-  connection.commit();
-  connection.close();
+  await cnx.commit();
+  await cnx.close();
+
   return {};
 };

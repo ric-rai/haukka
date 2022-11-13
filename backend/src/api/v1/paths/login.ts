54e161fff02f6df46cc2895563922de5ade87328
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-13 19:50:22 +0200

    update login endpoint to use account service

--- backend/src/api/v1/paths/login.ts
+++ backend/src/api/v1/paths/login.ts
@@ -1,53 +1,56 @@
 import { Operation } from "express-openapi";
 import * as jsonwebtoken from "jsonwebtoken";
+import { Account } from "../../../services/account/account";
 
-const { NODE_ENV, STATIC_SERVER_PORT, TARGET } = process.env;
+const { TARGET, AUTH_DOMAIN } = process.env;
 const JWT_SECRET = "goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu";
-const host = NODE_ENV === "development" ? `http://localhost:${STATIC_SERVER_PORT}` : "";
 
-export const GET: Operation = (req, res, next) => {
-  if (req.query.token) {
-    // Make HTTP request to laji to fetch user data
+export default function (account: Account) {
+  const GET: Operation = async (req, res, next) => {
+    if (req.query.token) {
+      if (typeof req.query.token !== "string")
+        return res.status(400).send(`Invalid query parameter 'token': ${req.query.token}`);
+
+      const result = await account.getPerson(req.query.token);
+      if (result === 404)
+        return res.status(404).send(`Unable to find user data for ${req.query.token}`);
+      if (result === 403) return res.status(403).send(`User doesn't have required role!`);
+      if (result === 500) return res.status(500).send(`Internal server error!`);
+      const { id, email } = result;
+      return res.redirect(`/#/?jwt=${jsonwebtoken.sign({ id, email }, JWT_SECRET)}`);
+    }
+
     return res.redirect(
-      `${host}/#/?jwt=${jsonwebtoken.sign(
-        {
-          user: "Test",
-          id: "123",
-          email: "test@domain.test",
-        },
-        JWT_SECRET
-      )}`
+      `${AUTH_DOMAIN}/laji-auth/login?` + `target=${TARGET}&redirectMethod=GET&next=/login`
     );
-  }
-  return res.redirect(
-    `https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?` +
-      `target=${TARGET}&redirectMethod=GET&next=`
-  );
-};
+  };
 
-const token = {
-  in: "query",
-  name: "token",
-  required: false,
-  schema: {
-    type: "string",
-  },
-};
-
-GET.apiDoc = {
-  summary: "Handles login after the user has been authenticated.",
-  operationId: "login",
-  parameters: [token],
-  responses: {
-    304: {
-      description: "Redirect to the root.",
-      content: {
-        "text/plain": {
-          schema: {
-            type: "string",
+  GET.apiDoc = {
+    summary: "Handles login after the user has been authenticated.",
+    operationId: "getPerson",
+    parameters: [
+      {
+        in: "query",
+        name: "token",
+        required: false,
+        schema: {
+          type: "string",
+        },
+      },
+    ],
+    responses: {
+      304: {
+        description: "Redirect to the root.",
+        content: {
+          "text/plain": {
+            schema: {
+              type: "string",
+            },
           },
         },
       },
     },
-  },
-};
+  };
+
+  return { GET };
+}

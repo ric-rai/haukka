f2a7cbb0c765eed73f369fb76ab0b661145924e8
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-11-19 18:30:57 +0200

    refactor and add getByName method

--- backend/src/services/observatory/observatory.ts
+++ backend/src/services/observatory/observatory.ts
@@ -1,15 +1,22 @@
 import oracledb = require("oracledb");
-import { parseStatements } from "../../utils";
+import { parseStatements, TupleToObject } from "../../utils";
 import * as path from "path";
-import type { MetadataService } from "../metadata/metadata";
+import type { Metadata, MetadataService } from "../metadata/metadata";
 import { locations } from "../../locations";
 
 const statements = parseStatements(path.join(__dirname, "./observatory.sql"));
 
-export type Observatory = Awaited<ReturnType<typeof Observatory>>;
+type ObservatoryRow = [number, Metadata, number, string];
+type Observatory = TupleToObject<ObservatoryRow, ["id", "metadata", "observatory", "name"]>;
+type Result = oracledb.Result<ObservatoryRow>;
 
-export const Observatory = async (pool: oracledb.Pool, metadataService: MetadataService) => {
+export type ObservatoryService = Awaited<ReturnType<typeof ObservatoryService>>;
+
+export const ObservatoryService = async (pool: oracledb.Pool, metadataService: MetadataService) => {
   const SQL = (cnx: oracledb.Connection) => ({
+    selectByName: async (name: string) => {
+      return (await cnx.execute(statements.SELECT_BY_NAME, { name })) as Result;
+    },
     insert: async (name: string, actions: string) => {
       return await cnx.execute(statements.INSERT, {
         metadata: await metadataService.create(cnx),
@@ -36,5 +43,18 @@ export const Observatory = async (pool: oracledb.Pool, metadataService: Metadata
   await cnx.commit();
   await cnx.close();
 
-  return {};
+  return {
+    getByName: async (name: string): Promise<Observatory | null> => {
+      const cnx = await pool.getConnection();
+      const { rows } = await SQL(cnx).selectByName(name);
+      await cnx.close();
+      if (!rows) return null;
+      return {
+        id: rows[0][0],
+        metadata: rows[0][1],
+        observatory: rows[0][2],
+        name: rows[0][3],
+      };
+    },
+  };
 };

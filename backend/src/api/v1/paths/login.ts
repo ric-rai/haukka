f972deef53029c0dc29a6ee74d9268bf8a1f0391
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-12-10 15:07:42 +0200

    use central api doc

--- backend/src/api/v1/paths/login.ts
+++ backend/src/api/v1/paths/login.ts
@@ -5,52 +5,25 @@ import { AccountService } from "../../../services/account";
 const { TARGET, AUTH_DOMAIN } = process.env;
 const JWT_SECRET = "goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu";
 
-export default function (accountService: AccountService) {
-  const GET: Operation = async (req, res, next) => {
-    if (req.query.token) {
-      if (typeof req.query.token !== "string")
-        return res.status(400).send(`Invalid query parameter 'token': ${req.query.token}`);
+export default function (accountService: AccountService): { GET: Operation } {
+  return {
+    GET: async (req, res, next) => {
+      if (req.query.token) {
+        if (typeof req.query.token !== "string")
+          return res.status(400).send(`Invalid query parameter 'token': ${req.query.token}`);
 
-      const result = await accountService.getAccount(req.query.token);
-      if (result === 404)
-        return res.status(404).send(`Unable to find user data for ${req.query.token}`);
-      if (result === 403) return res.status(403).send(`User doesn't have required role!`);
-      if (result === 500) return res.status(500).send(`Internal server error!`);
-      const { identity, email } = result;
-      return res.redirect(`/#/?jwt=${jsonwebtoken.sign({ identity, email }, JWT_SECRET)}`);
-    }
+        const result = await accountService.getAccount(req.query.token);
+        if (result === 404)
+          return res.status(404).send(`Unable to find user data for ${req.query.token}`);
+        if (result === 403) return res.status(403).send(`User doesn't have required role!`);
+        if (result === 500) return res.status(500).send(`Internal server error!`);
+        const { identity, email } = result;
+        return res.redirect(`/#/?jwt=${jsonwebtoken.sign({ identity, email }, JWT_SECRET)}`);
+      }
 
-    return res.redirect(
-      `${AUTH_DOMAIN}/laji-auth/login?` + `target=${TARGET}&redirectMethod=GET&next=/login`
-    );
-  };
-
-  GET.apiDoc = {
-    summary: "Handles login after the user has been authenticated.",
-    operationId: "getAccount",
-    parameters: [
-      {
-        in: "query",
-        name: "token",
-        required: false,
-        schema: {
-          type: "string",
-        },
-      },
-    ],
-    responses: {
-      304: {
-        description: "Redirect to the root.",
-        content: {
-          "text/plain": {
-            schema: {
-              type: "string",
-            },
-          },
-        },
-      },
+      return res.redirect(
+        `${AUTH_DOMAIN}/laji-auth/login?` + `target=${TARGET}&redirectMethod=GET&next=/login`
+      );
     },
   };
-
-  return { GET };
 }

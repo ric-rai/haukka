c40d96aafd06a2f6b84e5040c3e69b7a2056ee93
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-12-10 17:10:14 +0200

    change path

--- backend/scripts/generate.ts
+++ backend/scripts/generate.ts
@@ -1,4 +1,4 @@
-import { execSync, spawnSync } from "child_process";
+import { execSync } from "child_process";
 import * as fs from "fs";
 import { resolve } from "path";
 
@@ -10,5 +10,5 @@ execSync(`openapi --input ${input} --output ${output} --name ${name}`);
 
 const apiDoc = execSync(`js-yaml src/api/v1/apiDoc.yml`).toString();
 const typeExport = `export type ApiDoc = ${apiDoc}`;
-const path = resolve(__dirname, "../src/api/v1/generated/ApiDoc.ts");
+const path = resolve(__dirname, "../src/api/v1/apiDoc.ts");
 fs.writeFileSync(path, typeExport);

d6b5b9526c1caedbc34c135f7d8bab0204450105
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-12-10 16:59:24 +0200

    add generate script

---
+++ backend/scripts/generate.ts
@@ -0,0 +1,14 @@
+import { execSync, spawnSync } from "child_process";
+import * as fs from "fs";
+import { resolve } from "path";
+
+const input = `./src/api/v1/apiDoc.yml`;
+const output = `./src/api/v1/generated`;
+const name = `v1HaukkaClient`;
+
+execSync(`openapi --input ${input} --output ${output} --name ${name}`);
+
+const apiDoc = execSync(`js-yaml src/api/v1/apiDoc.yml`).toString();
+const typeExport = `export type ApiDoc = ${apiDoc}`;
+const path = resolve(__dirname, "../src/api/v1/generated/ApiDoc.ts");
+fs.writeFileSync(path, typeExport);
